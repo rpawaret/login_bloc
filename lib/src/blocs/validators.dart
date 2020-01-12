487f1882a2a4d494e39b646d496095494bf0380b
pawaret <pawaret.r@gmail.com>
2020-01-12 17:21:20 +0700

    153

---
+++ lib/src/blocs/validators.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+class Validators {
+  final validateEmail =
+      StreamTransformer<String, String>.fromHandlers(handleData: (email, sink) {
+    if (email.contains('@')) {
+      sink.add(email);
+    } else {
+      sink.addError('Enter a valid email');
+    }
+  });
+
+  final validatePassword = StreamTransformer<String, String>.fromHandlers(
+      handleData: (password, sink) {
+    if (password.length > 3) {
+      return sink.add(password);
+    } else {
+      return sink.addError('Password muse be at least 4 characters');
+    }
+  });
+}
