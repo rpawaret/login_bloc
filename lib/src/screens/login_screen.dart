30d6702065f6bf11ac8621ee98be19959c17bf47
pawaret <pawaret.r@gmail.com>
2020-01-12 14:19:01 +0700

    145

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -6,8 +6,38 @@ class LoginScreen extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(20),
       child: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(
+            margin: EdgeInsets.only(top: 25),
+          ),
+          submitButton()
+        ],
       ),
     );
   }
+}
+
+Widget emailField() {
+  return TextField(
+    keyboardType: TextInputType.emailAddress,
+    decoration:
+        InputDecoration(hintText: 'you@example.com', labelText: 'Email'),
+  );
+}
+
+Widget passwordField() {
+  return TextField(
+    obscureText: false,
+    decoration: InputDecoration(hintText: 'Password', labelText: 'Password'),
+  );
+}
+
+Widget submitButton() {
+  return RaisedButton(
+    child: Text('Login'),
+    color: Colors.blue,
+    onPressed: () {},
+  );
 }
