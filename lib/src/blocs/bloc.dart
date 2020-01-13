79f71b521681dff69b03e81a6be75fce348dfe88
pawaret <pawaret.r@gmail.com>
2020-01-13 22:28:56 +0700

    171

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import '../blocs/validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Bloc extends Object with Validators {
   final _email = StreamController<String>();
@@ -9,9 +10,11 @@ class Bloc extends Object with Validators {
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
 
-  //  Retrieve data from stream
+  // Retrieve data from stream
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
+  Stream<bool> get submitValid =>
+      Rx.combineLatest2(email, password, (e, p) => true);
 
   dispose() {
     _email.close();
