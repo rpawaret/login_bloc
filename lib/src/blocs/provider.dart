11b7e3a6dcd88fddeb2fab132103ebd5615cf6e2
pawaret <pawaret.r@gmail.com>
2020-01-13 21:35:28 +0700

    162

--- lib/src/blocs/provider.dart
+++ lib/src/blocs/provider.dart
@@ -3,13 +3,15 @@ import 'bloc.dart';
 
 class Provider extends InheritedWidget {
   final bloc = Bloc();
+
+  Provider({Key key, Widget child}) : super(key: key, child: child);
+
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) {
     return true;
   }
 
   static Bloc of(BuildContext context) {
-      return (context.inheritFromWidgetOfExactType(Provider) as Provider).bloc;
+    return (context.inheritFromWidgetOfExactType(Provider) as Provider).bloc;
   }
-
 }
